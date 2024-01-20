edbbc3b546c50ba8ad2e8a0eb621070b8372c5e4
Bruno Monteiro <montonurb@MacBook-Air-de-Bruno.local>
2024-01-20 09:22:34 -0300

    Motorista

---
+++ LocadoraVeiculo/src/Motorista.java
@@ -0,0 +1,47 @@
+/**
+ *
+ * @author montonurb
+ */
+public class Motorista {
+    private String nome;
+    private String cpf;
+    private String contato;
+    
+    public Motorista(){
+    }
+
+    public Motorista(String nome, String cpf, String contato) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.contato = contato;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    @Override
+    public String toString() {
+        return "Motorista{" + "nome=" + nome + ", cpf=" + cpf + ", contato=" + contato + '}';
+    }
+}
--- LocadoraVeiculo/src/Principal.java
+++ LocadoraVeiculo/src/Principal.java
@@ -7,24 +7,32 @@ import javax.swing.JOptionPane;
  */
 public class Principal {
     public static void main(String[] args) {
+        Motorista motorista = new Motorista();
+        
+        motorista.setNome("Abimar");
+        motorista.setCpf("098.098.000-99");
+        motorista.setContato("(86) 99910-0910");
+
+        JOptionPane.showMessageDialog(null, motorista.toString());
+        
 //        String mensagem;
-        Carro carro = new Carro();
-        carro.cor = "Amarelo";
-        carro.modelo = "Fiat";
-        carro.tipoCombustivel = "Gasolina";
-        carro.valor = 1000.00;
-        carro.ligar();
-        carro.mudarMarcha();
-        
-        while(carro.marcha > 0 && carro.marcha <= 6) {
-            carro.acelerarManualmente(15);
-            System.out.println(carro.mensagem());
-            carro.mudarMarcha();
-        }
-        
-        carro.mensagem();
-        System.out.println("Valor do carro: " + carro.valor);
-        System.out.println("Valor do carro com desconto: " + carro.venderCarro(10));
+//        Carro carro = new Carro();
+//        carro.cor = "Amarelo";
+//        carro.modelo = "Fiat";
+//        carro.tipoCombustivel = "Gasolina";
+//        carro.valor = 1000.00;
+//        carro.ligar();
+//        carro.mudarMarcha();
+//        
+//        while(carro.marcha > 0 && carro.marcha <= 6) {
+//            carro.acelerarManualmente(15);
+//            System.out.println(carro.mensagem());
+//            carro.mudarMarcha();
+//        }
+//        
+//        carro.mensagem();
+//        System.out.println("Valor do carro: " + carro.valor);
+//        System.out.println("Valor do carro com desconto: " + carro.venderCarro(10));
         
 //        mensagem = "Carro de modelo " + carro.modelo
 //                + ", cor " + carro.cor + ", combustível do tipo "
